0258a12258d165ba799ef34ccd8ef1caf34b0a1b
Hyunjwun <siyohando@gmail.com>
2026-02-13 18:47:47 +0700

    Implement background music on user interaction
    Added background music functionality that plays on the first user click.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
   const envelope = $("#envelope");
+  const music = document.getElementById("bg-music");
+
+  music.volume = 0.7;
+
+  // Fungsi play sekali saja
+  function startMusic() {
+    music.play().catch(function (err) {
+      console.log("Autoplay blocked:", err);
+    });
+
+    // Hapus listener supaya tidak play berulang
+    document.removeEventListener("click", startMusic);
+  }
+
+  // Tunggu klik pertama user
+  document.addEventListener("click", startMusic);
 
   $("#open").click(function () {
     envelope.removeClass("close").addClass("open");
